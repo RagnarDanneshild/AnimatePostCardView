655a6a3dc81e8746eee1a1cf525ef90e993efeef
RagnarDanneshild <paveltumash@yandex.ru>
2015-12-18 18:00:48 +0300

    Initialize project

--- PostCard/views.py
+++ PostCard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
 
+from PostCard.models import *
 # Create your views here.
+
+def home(request):
+    return render(
+    request,
+    'index.html'
+
+
+)
+
