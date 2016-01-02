067071b5e85b35ead86e09b6efbc18e97e280e76
RagnarDanneshild <paveltumash@yandex.ru>
2016-01-02 17:19:37 +0300

    Scroll Update bd, by  postcard, created by one young lady

--- PostCard/static/scripts/InfinityScroll.js
+++ PostCard/static/scripts/InfinityScroll.js
@@ -1,8 +1,10 @@
 /**
  * Created by Pawel on 30.12.2015.
  */
-var coff=4;
+var coff=2;
 $(document).ready(function(){
+
+    if (window.location.pathname!='/')return  0;
   var screenHeight = $(window).height();
 
   $(window).scroll(function()
@@ -11,7 +13,7 @@ $(document).ready(function(){
       var divHeight = $("#imgContainer").height();
       var totalHeight = screenHeight + scroll;
       var left = divHeight - totalHeight;
-      if (left < 10)
+      if (left < 10 && left>-120)
         moreData();
     });
 });
