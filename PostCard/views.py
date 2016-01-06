85b05609053cdfd8f4cf714561999b885d5bc28f
hellalien <maxag1@mail.by>
2016-01-06 13:57:58 +0300

    smthing

--- PostCard/views.py
+++ PostCard/views.py
@@ -14,31 +14,33 @@ import json
 
 
 class Home(TemplateView):
-   template_name='index.html'
+   template_name = 'index.html'
 
 
 def profile(request):
-    userProfile=UserInfo.objects.get(user=request.user)
-    form=UserProfileForm
-    form.instance=userProfile
+    userProfile = UserInfo.objects.get(user=request.user)
+    form = UserProfileForm
+    form.instance = userProfile
     return render(
         request,
         'profile.html',
-        context_instance = RequestContext(request,
+        context_instance=RequestContext(request,
         {"form": form,
-            'userInfo':userProfile
+            'userInfo': userProfile
         }
-          )
+        )
     )
 
 
 class edit(TemplateView):
-   template_name='edit.html'
+   template_name = 'edit.html'
 
 
 def save(request):
+    print('a')
     if request.is_ajax():
-        post_card = PostCard(user=request.user, canvas=request.POST['json'], picture_url=request.POST['url'],name=request.POST['name'])
+        print('b')
+        post_card = PostCard(user=request.user, canvas=request.POST['json'], picture_url=request.POST['url'], name=request.POST['name'])
         if PostCard.objects.filter(name=request.POST['name']).exists():
             oldpost_card = PostCard.objects.get(name=request.POST['name'])
             if oldpost_card.user == request.user:
@@ -46,15 +48,16 @@ def save(request):
                 oldpost_card.picture_url = request.POST['url']
                 oldpost_card.save()
             else:
-                HttpResponse('this is not yourst postcard')
+                HttpResponse('this is not yours postcard')
         else:
             post_card.save()
+            print('d')
     return HttpResponse('it s ok')
 
 
 def savetemplate(request):
     if request.is_ajax():
-        template = Template(canvas=request.POST['json'], template_url=request.POST['url'],name=request.POST['name'])
+        template = Template(canvas=request.POST['json'], template_url=request.POST['url'], name=request.POST['name'])
         template.save()
     return HttpResponse('it s ok')
 
@@ -62,16 +65,16 @@ def savetemplate(request):
 def edit(request, templnum='', id=0):
     if templnum != '':
         if request.is_ajax():
-            s = serializers.serialize('json',[Template.objects.get(name=templnum)])
+            s = serializers.serialize('json', [Template.objects.get(name=templnum)])
             return HttpResponse(s, content_type='application/json')
         else:
             return render(request,'edit.html')
     elif templnum == '' and id != 0:
         if request.is_ajax():
-            s=serializers.serialize('json',[PostCard.objects.get(id=id)])
+            s = serializers.serialize('json', [PostCard.objects.get(id=id)])
             return HttpResponse(s, content_type='application/json')
         else:
-            return render(request,'edit.html')
+            return render(request, 'edit.html')
 
 
 def save_post_card(request):
@@ -89,8 +92,6 @@ def getlist(request, num=-1):
             data = serializers.serialize("json", tlist)
         else:
             slist = PostCard.objects.all().order_by('-creation_date')
-            for item in slist:
-                print (item.id)
             if request.GET.get('user')==None:
                 data = serializers.serialize("json", slist[int(num): int(num)+2])
             else:
@@ -103,18 +104,18 @@ def showPostCard(request,id=1):
         s=serializers.serialize('json',[PostCard.objects.get(id=id)])
         return HttpResponse(s, content_type='application/json')
     else:
-        return render(request,'PostCardView.html')
+        return render(request, 'PostCardView.html')
 
 
 def rate(request):
-    data={}
+    data = {}
     if request.is_ajax():
-        user=request.user
-        if(request.POST):
-            rating =int(request.POST['value'])
-            postcard=PostCard.objects.get(id=int(request.POST['id']))
-            obj,create=PostCardRating.objects.update_or_create(user=user,post_card=postcard,defaults={'rate':rating})
-            if(create):
+        user = request.user
+        if request.POST:
+            rating = int(request.POST['value'])
+            postcard = PostCard.objects.get(id=int(request.POST['id']))
+            obj,create = PostCardRating.objects.update_or_create(user=user,post_card=postcard,defaults={'rate':rating})
+            if create:
                 postcard.update_rating(rating,create)
             else:
                 postcard.update_rating(rating-obj.prev_rate,create)
@@ -123,16 +124,16 @@ def rate(request):
             data['vote_num']=postcard.like_num
 
         if request.GET:
-            postcard=PostCard.objects.get(id=int(request.GET.get('id')))
-            obj=PostCardRating.objects.filter(user=user,post_card=postcard)
-            data['rating']=postcard.rating
+            postcard = PostCard.objects.get(id=int(request.GET.get('id')))
+            obj = PostCardRating.objects.filter(user=user, post_card=postcard)
+            data['rating'] = postcard.rating
             data['vote_num'] = postcard.like_num
             if obj.exists():
-                data['user_rate']=obj[0].rate
+                data['user_rate'] = obj[0].rate
             else:
-                data['user_rate']=0
+                data['user_rate'] = 0
 
-    return HttpResponse(json.dumps(data), content_type = "application/json")
+    return HttpResponse(json.dumps(data), content_type="application/json")
 
 
 def checkbudges(request):
