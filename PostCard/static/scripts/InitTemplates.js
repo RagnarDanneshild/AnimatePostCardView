a69b3e27f8e703fa869faafe596af73ed530c7f6
RagnarDanneshild <paveltumash@yandex.ru>
2016-01-07 15:09:53 +0300

    Change Templates LittlBit

---
+++ PostCard/static/scripts/InitTemplates.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Pawel on 07.01.2016.
+ */
+   function inittemplates()
+    {
+        $.get('/getListOfPicture/templates/',function(data){
+            $.each( data, function( i, item ) {
+                 getImage(item.fields.template_url,function(url){
+                    $( "#templateContainer").append(getView(url,item.pk,item.fields.name,'true'));
+                });
+            });
+        });
+    };
